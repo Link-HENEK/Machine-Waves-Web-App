caf729b31d5ea28a511a5e37939c377be566c031
Link HENEK <Link.henek@gmail.com>
2025-04-30 22:09:18 +0200

    First Commit from ASP NET Core project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Machine_Waves_Web_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
